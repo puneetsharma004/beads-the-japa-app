ae288c2af7c562106585b5cd48b09975a6a10b44
Puneet <puneetkfl147@gmail.com>
2025-08-22 23:21:30 +0530

    h screen

--- src/components/CounterDisplay.jsx
+++ src/components/CounterDisplay.jsx
@@ -11,7 +11,7 @@ export default function CounterDisplay({ count, isStealthMode }) {
   }, [count])
 
   return (
-    <div className="flex flex-col items-center justify-center my-4">
+    <div className="flex flex-col items-center justify-center my-4 gap-4">
       <h3>
         <span className="text-[#212830] flex items-center gap-1 font-bold">108 <RxCross2/> {roundsChanted} </span>
       </h3>
